2bce2f673904e2b0d7927657a016154d6bb79a70
ramp4 <rampatv13@gmail.com>
2019-12-21 21:43:15 +0300

    fix: watnings

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -1,3 +1,4 @@
+/* eslint-disable func-names */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 /* ************************************************************************************************
